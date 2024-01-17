6bb5ded1ef040faed538e6fa3451b69000456b59
Jose Rodrigo <rodrigovargasv80@gmail.com>
2024-01-17 08:31:54 -0300

    Criado e implementando classe e métodos do Unit Of Work.

---
+++ src/DesafioRodonaves/DesafioRodonaves.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿
+namespace DesafioRodonaves.Domain.Interfaces
+{
+    public interface IUnitOfWork<TDbContext>
+    {
+
+        Task<bool> Commit();
+
+        void Rollback();
+    }
+}
---
+++ src/DesafioRodonaves/DesafioRodonaves.Infra.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using DesafioRodonaves.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesafioRodonaves.Infra.Data.UnitOfWord
+{
+    public class UnitOfWork<TDbContext> : IUnitOfWork<TDbContext> where TDbContext : DbContext
+    {
+        private readonly TDbContext _context;
+
+        public UnitOfWork(TDbContext context)
+        {
+            _context = context;
+        }
+
+        // Implementação do unit of work com o método saveChanges
+        public async Task<bool> Commit()
+          => await _context.SaveChangesAsync() > 0;
+
+        public void Rollback()
+        {
+            // não faz nada;
+        }
+    }
+}
